38efb3b9a309872498c2d7f42baa0e72c929bb78
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-12-03 17:12:32 +0700

    Add

--- app/src/main/java/com/hoc/pagination_mvi/ui/main/MainFragment.kt
+++ app/src/main/java/com/hoc/pagination_mvi/ui/main/MainFragment.kt
@@ -4,23 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import android.widget.Toast.LENGTH_SHORT
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.hoc.pagination_mvi.R
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
+import com.hoc.pagination_mvi.ui.main.MainContract.ViewIntent
 import dagger.android.support.AndroidSupportInjection
-import dagger.android.support.DaggerFragment
-import dagger.android.support.HasSupportFragmentInjector
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import javax.inject.Inject
 
 class MainFragment : Fragment() {
   @Inject
   lateinit var factory: ViewModelProvider.Factory
   private val mainVM by viewModels<MainVM> { factory }
+  private val compositeDisposable = CompositeDisposable()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     AndroidSupportInjection.inject(this)
@@ -35,6 +35,22 @@ class MainFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    Toast.makeText(requireContext(), "$factory $mainVM", LENGTH_SHORT).show()
+    bindVM()
+  }
+
+  private fun bindVM() {
+    mainVM.stateD.observe(viewLifecycleOwner, Observer {
+
+    })
+    mainVM.processIntents(
+      Observable.mergeArray(
+        Observable.just(ViewIntent.Initial)
+      )
+    ).addTo(compositeDisposable)
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    compositeDisposable.clear()
   }
 }
--- app/src/main/java/com/hoc/pagination_mvi/ui/main/MainVM.kt
+++ app/src/main/java/com/hoc/pagination_mvi/ui/main/MainVM.kt
@@ -1,12 +1,54 @@
 package com.hoc.pagination_mvi.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.hoc.pagination_mvi.di.ApplicationScope
 import com.hoc.pagination_mvi.domain.usecase.GetPhotosUseCase
+import com.hoc.pagination_mvi.ui.main.MainContract.ViewIntent
+import com.hoc.pagination_mvi.ui.main.MainContract.ViewState
+import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.ofType
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.PublishSubject
 import javax.inject.Inject
 
 class MainVM @Inject constructor(
   private val getPhotosUseCase: GetPhotosUseCase
 ) : ViewModel() {
+  private val _stateD =
+    MutableLiveData<ViewState>().apply { value = ViewState.initial() }
 
+  val stateD: LiveData<ViewState> get() = _stateD
+
+  private val intentS = PublishSubject.create<ViewIntent>()
+
+  private val compositeDisposable = CompositeDisposable()
+
+  fun processIntents(intents: Observable<ViewIntent>) = intents.subscribe(intentS::onNext)!!
+
+  init {
+    intentS
+      .compose(intentFilter)
+      .subscribeBy { }
+      .addTo(compositeDisposable)
+  }
+
+  override fun onCleared() {
+    super.onCleared()
+    compositeDisposable.dispose()
+  }
+
+  private companion object {
+    val intentFilter = ObservableTransformer<ViewIntent, ViewIntent> { intents ->
+      intents.publish { shared ->
+        Observable.mergeArray(
+          shared.ofType<ViewIntent.Initial>().take(1),
+          shared.filter { it !is ViewIntent.Initial }
+        )
+      }
+    }
+  }
 }
